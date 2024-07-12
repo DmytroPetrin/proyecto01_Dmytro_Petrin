44dfcb2d578b981249cbf02f80b468221b496875
Dmytro Petrin <chichin91@hotmail.com>
2024-07-12 21:15:10 +0200

    git ignore added

---
+++ servidor/config.js
@@ -0,0 +1,9 @@
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:4200"
+
+export const DB_HOST = process.env.DB_HOST || "localhost"
+export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_DATABASE = process.env.DB_DATABASE || 'pizzeria'
+export const DB_USER = process.env.DB_USER || 'dima'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '123456'
+
+export const PORT = 3000
